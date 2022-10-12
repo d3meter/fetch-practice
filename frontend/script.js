37fd91221d5b56d8e2a39eb099444a824fd0af1e
d3meter <mark.demeter3@gmail.com>
2022-10-12 11:11:35 +0200

    finish practice

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
-let fetchUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+const apiKey = "QYSDCrsuNdQpx6YY9Yg2eO9RBWDIVwpWkwhwYWi8";
+let apiDate = "1996-03-01";
+let fetchUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${apiDate}`;
 
 /* fetchUrl(fetchUrl)
     .then(function (response) {
@@ -9,6 +11,29 @@ let fetchUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
     });
  */
 
-fetch(fetchUrl)
+/* fetch(fetchUrl)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => console.log(responseJson)); */
+
+async function fetchNasa () {
+    const response = await fetch(fetchUrl);
+/*     console.log(response); */
+    const responseJson = await response.json();
+/*     console.log(responseJson); */
+    return responseJson;
+}
+
+async function loadEvent () {
+    let data = await fetchNasa();
+
+    console.log("data: ", data);
+
+    const rootElement = document.querySelector("#root");
+    rootElement.insertAdjacentHTML("beforeend", `
+        <h1>${data.title}</h1>
+        <h2>${data.date}</h2>
+        <p>${data.explanation}</p>
+        <img src="${data.url}">
+    `)
+}
+window.addEventListener("load", loadEvent)
